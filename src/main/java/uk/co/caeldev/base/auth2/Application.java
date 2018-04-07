393bfc7208a437a68c8d0f2c139f8a9f17d606f7
caelwinner <adolfoecs@gmail.com>
2018-04-07 11:42:40 +0100

    initial Application Cass

---
+++ src/main/java/uk/co/caeldev/base/auth2/Application.java
@@ -0,0 +1,14 @@
+package uk.co.caeldev.base.auth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
