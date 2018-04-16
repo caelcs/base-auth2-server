4147498a4a06661c5f97ce61e82782d93434ab05
Adolfo Secchi <adolfo.secchi.external@ynap.com>
2018-04-16 13:04:05 +0100

    wip9

--- src/main/java/uk/co/caeldev/base/auth2/config/MainConfiguration.java
+++ src/main/java/uk/co/caeldev/base/auth2/config/MainConfiguration.java
@@ -2,12 +2,10 @@ package uk.co.caeldev.base.auth2.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import uk.co.caeldev.springsecuritymongo.config.EnableSecurityMongo;
 
 @Configuration
 @EnableSecurityMongo
-@EnableResourceServer
 @Import(value = { AuthorizationServerConfiguration.class })
 public class MainConfiguration {
 }
